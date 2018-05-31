a0bc1e4174c7597b5780fb55ac138f12fe58b3e7
Yan <danteivsinero@mail.ru>
2018-05-31 21:56:31 +0300

    First run

---
+++ src/Main.elm
@@ -0,0 +1,62 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, program, text)
+
+
+-- MODEL
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "Hello", Cmd.none )
+
+
+
+-- MESSAGES
+
+
+type Msg
+    = NoOp
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text model ]
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIBTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
