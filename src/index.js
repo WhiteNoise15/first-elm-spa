00acf50ed4a3a5e4ed32b2bd274037712c364d67
Yan <danteivsinero@mail.ru>
2018-05-31 20:43:46 +0300

    Added dependencies

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'ace-css/css/ace.css';
+import 'font-awesome/css/font-awesome.css';
+
+// Require index.html so it gets copied to dist
+import './index.html';
+
+import Elm from './Main.elm';
+
+const mountNode = document.getElementById('main');
+
+// .embed() can take an optional second argument. This would be an object describing the data we need to start a program, i.e. a userID or some token
+const app = Elm.Main.embed(mountNode);
